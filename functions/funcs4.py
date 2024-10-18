4d61888da6364f1ce8848918421c8d5e991faca5
Олег <cosmos65850@mail.ru>
2024-10-18 19:18:35 +0300

    change2 homework

--- functions/funcs4.py
+++ functions/funcs4.py
@@ -1,7 +1,7 @@
 def process_order(*args, **kwargs):
     print("Продукты:", *args)
     print("Информация:")
-    for key, value in kwargs:
+    for key, value in kwargs.items():
         print(f"{key}: {value}")
 
 process_order('Пицца', 'Суши', 'Суши' , name='Райан', surname='Гослинг')
